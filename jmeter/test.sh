f2d8d4e7d0739291527ffa7cde8a4f440802cc57
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 17:11:18 -0500

    se sube reporte

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -41,13 +41,11 @@ ls -l $T_DIR
 echo "==== HTML Test Report ===="
 echo "See HTML test report in $R_DIR/index.html"
 
-
-# echo "===================== Descarga de AzCopy"
-# wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
-# echo "===================== Descarga de Index"
-# ./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
-# echo "===================== Carga de reporte a https://resulttests.blob.core.windows.net/%24web/rep/$NOMBREREP"
-# ./azcopy copy "./report" "https://resulttests.blob.core.windows.net/%24web/rep/$NOMBREREP?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
-
-
+echo "===================== Descarga de AzCopy"
+wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
+echo "===================== Descarga de Index"
+./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
 echo "===================== Carga de reporte a https://resulttests.blob.core.windows.net/%24web/rep/$nombrereporte"
+./azcopy copy "./report" "https://resulttests.blob.core.windows.net/%24web/rep/$nombrereporte?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
+nuevoReporte="Build $nombrereporte"
+uriReporte=""
