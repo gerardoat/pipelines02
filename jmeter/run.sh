85ffd0249b16b4b4e41b6a64879620cb51d86ca9
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 16:32:51 -0500

    se mueve uso de azcopy a run.sh

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -7,3 +7,7 @@ echo "$ROOTPATH"
 docker stop $NAME > /dev/null 2>&1
 docker rm $NAME > /dev/null 2>&1
 docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
+echo "Publicando resultados..."
+wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
+./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
+ls -l
