885ca33db7f49cdf6516907ab6f6c40d3468846e
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 17:43:39 -0500

    refactorizaciòn de datos de SAS

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -2,6 +2,8 @@ rootPath=$1
 testFile=$2
 host=$3
 idReporte=$4
+fqdnContenedor="https://resulttests.blob.core.windows.net/%24web"
+sasContenedor="sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D"
 
 echo "======================== PARAMETROS ========================"
 echo "Root path: $rootPath"
@@ -44,11 +46,11 @@ echo "See HTML test report in $R_DIR/index.html"
 echo "===================== Descarga de AzCopy"
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
 echo "===================== Descarga de Index"
-./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
+./azcopy copy "$fqdnContenedor/index.html?$sasContenedor" "./index.html"
 echo "===================== Carga de reporte a https://resulttests.blob.core.windows.net/%24web/rep/$idReporte"
-./azcopy copy "./report" "https://resulttests.blob.core.windows.net/%24web/rep/$idReporte?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
+./azcopy copy "./report" "$fqdnContenedor/rep/$idReporte?$sasContenedor" --recursive=true
 liReporte="<li><a href='rep/$idReporte/report/index.html'>Build $idReporte</a></li>"
 echo "===================== Actualizando Index con $liReporte"
 echo "$liReporte" >> ./index.html
 echo "===================== Carga de Index"
-./azcopy copy "./index.html" "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
+./azcopy copy "./index.html" "$fqdnContenedor/index.html?$sasContenedor" --recursive=true
