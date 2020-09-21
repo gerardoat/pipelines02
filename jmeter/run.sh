c3100265aa548c9e3af2e8dd1845fbb16a7de329
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 16:48:08 -0500

    correcciòn diagonal

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -13,4 +13,4 @@ wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.
 echo "===================== Descarga de Index"
 ./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
 echo "===================== Carga de rporte"
-./azcopy copy ".\report" "https://resulttests.blob.core.windows.net/%24web/rep/$NOMBREREP?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
+./azcopy copy "./report" "https://resulttests.blob.core.windows.net/%24web/rep/$NOMBREREP?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
