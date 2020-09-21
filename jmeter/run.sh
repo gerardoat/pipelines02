c3e632c98557fb0f9762a5dafd83988893f82e14
Gerardo Garcia <g.garciac@outlook.com>
2020-09-20 23:12:03 -0500

    Se deshacen echos

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -3,16 +3,12 @@
 #
 # Run JMeter Docker image with options
 
-echo "Iniciando JMeter Docker Image"
 NAME="jmetertest"
 IMAGE="justb4/jmeter:latest"
 ROOTPATH=$1
 
 echo "$ROOTPATH"
 # Finally run
-echo "Docker STOP..."
-docker stop $NAME > /dev/null 2>&1
-echo "Docker RM..."
-docker rm $NAME > /dev/null 2>&1
-echo "Docker RUN..."
-docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
+# docker stop $NAME > /dev/null 2>&1
+# docker rm $NAME > /dev/null 2>&1
+# docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
