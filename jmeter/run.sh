acba3d6034e25a459c13367a6016892c33427a3f
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 16:40:27 -0500

    Se sube reporte con azcopy

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -1,6 +1,7 @@
 NAME="jmetertest"
 IMAGE="justb4/jmeter:latest"
 ROOTPATH=$1
+NOMBREREP=$4
 
 echo "$ROOTPATH"
 
@@ -10,4 +11,4 @@ docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
 echo "Publicando resultados..."
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
 ./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
-ls -l
+./azcopy copy ".\report" "https://resulttests.blob.core.windows.net/%24web/rep/$NOMBREREP?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
