3c1fef8f918166b4ea43d5715c96f43c29097ee5
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 17:21:28 -0500

    Actualización de Index

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -1,13 +1,13 @@
 rootPath=$1
 testFile=$2
 host=$3
-nombrereporte=$4
+idReporte=$4
 
 echo "======================== PARAMETROS ========================"
 echo "Root path: $rootPath"
 echo "Test file: $testFile"
 echo "Host: $host"
-echo "Nombre reporte: $nombrereporte"
+echo "Nombre reporte: $idReporte"
 
 # echo "Permisos para archivo $rootPath/run.sh"
 # chmod +x $rootPath/run.sh
@@ -45,7 +45,8 @@ echo "===================== Descarga de AzCopy"
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy.tar.gz --strip-components=1
 echo "===================== Descarga de Index"
 ./azcopy copy "https://resulttests.blob.core.windows.net/%24web/index.html?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" "./index.html"
-echo "===================== Carga de reporte a https://resulttests.blob.core.windows.net/%24web/rep/$nombrereporte"
-./azcopy copy "./report" "https://resulttests.blob.core.windows.net/%24web/rep/$nombrereporte?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
-nuevoReporte="Build $nombrereporte"
-uriReporte=""
+echo "===================== Carga de reporte a https://resulttests.blob.core.windows.net/%24web/rep/$idReporte"
+./azcopy copy "./report" "https://resulttests.blob.core.windows.net/%24web/rep/$idReporte?sv=2019-10-10&st=2020-09-20T18%3A38%3A00Z&se=2020-12-31T19%3A38%3A00Z&sr=c&sp=rwl&sig=PA9SPfvV5HeCaSEMR6NL4HCfw%2FWNU%2Fkq5DJVr1DfpS4%3D" --recursive=true
+liReporte="<li><a href='rep/$idReporte/report/index.html'>Build $idReporte</a></li>"
+echo "===================== Actualizando Index con $liReporte"
+echo $uriReporte >> ./index.html
