6919f8080c37f86b38c44d7795e24ba821135264
Gerardo Garcia <g.garciac@outlook.com>
2020-09-20 23:14:24 -0500

    solo con docker run

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -11,4 +11,4 @@ echo "$ROOTPATH"
 # Finally run
 # docker stop $NAME > /dev/null 2>&1
 # docker rm $NAME > /dev/null 2>&1
-# docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
+docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
