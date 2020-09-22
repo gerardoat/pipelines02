110ebf63e2efdbbe4152fc3bd17b2f9142c09507
Gerardo Garcia <g.garciac@outlook.com>
2020-09-21 21:41:16 -0500

    prueba

--- infra/waitForApp.sh
+++ infra/waitForApp.sh
@@ -2,16 +2,16 @@ uri=$1
 
 echo "========== Iniciando espera de respuesta para "uri ..."
 
-while true
-do
-  STATUS=$(curl -s -o /dev/null -w '%{http_code}' $uri)
-  if [ $STATUS -eq 200 ]; then
-    echo "App OK"
-    break
-  else
-    echo "App no disponible aún: $STATUS "
-  fi
-  sleep 3
-done
+# while true
+# do
+#   STATUS=$(curl -s -o /dev/null -w '%{http_code}' $uri)
+#   if [ $STATUS -eq 200 ]; then
+#     echo "App OK"
+#     break
+#   else
+#     echo "App no disponible $STATUS "
+#   fi
+#   sleep 3
+# done
 
 echo "==========  Fin de la espera"
